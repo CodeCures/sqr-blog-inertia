79b50b3ac9cc599cf07f45b8cef33bb09cae6972
CodeCures <couragesblog@gmail.com>
2022-05-30 17:17:55 +0100

    Define application routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::middleware('guest')->group(function () {
+    Route::view('/login', 'auth.login')->name('login');
+    Route::view('/register', 'auth.register')->name('register');
 });
+
+Route::controller(AuthController::class)->prefix('auth')->group(function() {
+    Route::post('/login', 'login')->name('auth.login');
+    Route::post('/register', 'register')->name('auth.register');
+});
+
+Route::controller(PostController::class)->group(function (){
+    Route::get('/', 'index')->name('posts.index');
+    Route::get('/posts/{post:slug}', 'show')->name('posts.show');
+});
+
+
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+    Route::resource('post', PostController::class)->except(['index', 'show']);
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
+
+
