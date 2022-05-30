8bd2cc9d6a07b4d1c41e227703a2d9fc759b0b75
CodeCures <couragesblog@gmail.com>
2022-05-30 15:56:13 +0100

    Define admin priviledges
    privilages
    - update-post
    - delete-post

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Enums\UserRole;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-post', function() {
+            return optional(auth()->user())->role == UserRole::Admin;
+        });
+
+        Gate::define('delete-post', function() {
+            return optional(auth()->user())->role == UserRole::Admin;
+        });
     }
 }
