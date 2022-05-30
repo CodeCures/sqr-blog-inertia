f13ba8dc5539dc797198959125f6494045d0fcde
CodeCures <couragesblog@gmail.com>
2022-05-30 14:53:00 +0100

    Add QueryCacheable trait
    cache every post query for a given amount of time

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Post extends Model
 {
     use HasFactory;
+
+    use HasFactory, QueryCacheable;
+
+    /**
+     * Specify the amount of time to cache queries.
+     * Do not specify or set it to null to disable caching.
+     *
+     * @var int|\DateTime
+     */
+    public $cacheFor = 3600;
 }
