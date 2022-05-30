9794d996e599b9ce2c5dc86d4de5a4aeec6aba8e
CodeCures <couragesblog@gmail.com>
2022-05-30 17:31:01 +0100

    Create form header component

---
+++ resources/views/components/form-header.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <svg class="lock" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"></path></svg>
+</div>
+<h1 class="pt-5 font-body text-4xl font-semibold text-primary dark:text-white md:text-5xl lg:text-6xl mb-3">
+  {{ $title }}
+</h1>
+<div class="pr-2 pt-3 sm:pt-0">
+  <p class="font-body text-xl font-light text-primary dark:text-white">
+    {{ $description }}
+  </p>
+</div>
