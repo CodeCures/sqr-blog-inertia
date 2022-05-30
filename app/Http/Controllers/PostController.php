01255abbf1ceeda36fec4725b58692d703cdd95e
CodeCures <couragesblog@gmail.com>
2022-05-30 17:13:51 +0100

    Create post controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Post\CreatePost;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use App\Services\PostService;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = PostService::getPosts();
+        return view('home', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Redirect
+     */
+    public function store(PostRequest $request, CreatePost $createPost)
+    {
+        $createPost->execute($request->validated());
+
+        return to_route('dashboard')->with('msg', 'post created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('post.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $this->authorize('update-post');
+
+        return view('post.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        $this->authorize('update-post');
+
+        $post->update($request->validated());
+
+        return to_route('dashboard')->with('msg', 'post updated successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $this->authorize('delete-post');
+
+        $post->delete();
+
+        return to_route('dashboard')->with('msg', 'post deleted successfully!');
+    }
+}
