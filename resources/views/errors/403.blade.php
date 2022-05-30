5b21be0a6723d95730c0718767b4483563e5da69
CodeCures <couragesblog@gmail.com>
2022-05-30 17:27:05 +0100

    Create 403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <div class="container mx-auto">
+      <div class="py-16 lg:py-20">
+        <div>
+          <img class="w-full" src="{{ asset('assets/img/403.gif') }}" alt="icon envelope" />
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
