dc10f53a5426829f54495d3d27b1174c8a49764a
CodeCures <couragesblog@gmail.com>
2022-05-30 17:33:02 +0100

    Create pagination links compoent

---
+++ resources/views/components/pagination-link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => "group ml-3 flex cursor-pointer items-center border-2 border-primary px-3 py-1 font-body font-medium text-primary transition-colors hover:border-secondary hover:text-secondary dark:border-green-light dark:text-white dark:hover:border-secondary dark:hover:text-secondary"]) }}>
+    {{ $slot }}
+</a>
