32ddd3051428374a10cd541e8b6838d9e21ef1a9
CodeCures <couragesblog@gmail.com>
2022-05-30 20:41:07 +0100

    Retreive post data relating to user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PostService;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = PostService::getPosts();
+
+        return view('dashboard', compact('posts'));
+    }
 }
