08a0c9720c0cbe64d367b295db850508784ff3bb
CodeCures <couragesblog@gmail.com>
2022-05-30 15:38:34 +0100

    Define Post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\PostState;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence(6);
+
+        return [
+            'user_id'       => 1,
+            'title'         => $title,
+            'slug'         => Str::slug($title),
+            'description'   => $this->faker->sentence(50),
+            'state'         => PostState::Published,
+            'published_at' => now()
+        ];
+    }
+}
