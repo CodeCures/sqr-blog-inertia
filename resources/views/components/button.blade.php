a671bef92f8259722d016cd1eed734645d05e6a8
CodeCures <couragesblog@gmail.com>
2022-05-31 13:01:16 +0100

    Add dusk selector

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['class' => 'block bg-secondary px-10 py-4 text-center font-body text-xl font-semibold text-white transition-colors hover:bg-green sm:inline-block sm:text-left sm:text-2xl']) }}>
+<button dusk="submit-button" {{ $attributes->merge(['class' => 'block bg-secondary px-10 py-4 text-center font-body text-xl font-semibold text-white transition-colors hover:bg-green sm:inline-block sm:text-left sm:text-2xl']) }}>
     {{ $slot }}
 </button>
