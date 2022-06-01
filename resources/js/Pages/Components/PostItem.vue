dd60fb3dd1c3e4c1fc756512817a21a7ef272c81
CodeCures <couragesblog@gmail.com>
2022-06-01 09:19:29 +0100

    Create post item component

---
+++ resources/js/Pages/Components/PostItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col items-start col-span-12 overflow-hidden shadow-sm rounded-xl md:col-span-6 lg:col-span-4">
+        <a href="#_" class="block transition duration-200 ease-out transform hover:scale-110">
+            <img class="object-cover w-full shadow-sm max-h-56"
+                src="https://cdn.devdojo.com/images/may2021/workstation.jpg">
+        </a>
+        <div
+            class="relative flex flex-col items-start px-6 bg-white border border-t-0 border-gray-200 py-7 rounded-b-2xl">
+            <div
+                class="bg-indigo-400 absolute top-0 -mt-3 flex items-center px-3 py-1.5 leading-none w-auto inline-block rounded-full text-xs font-medium uppercase text-white inline-block">
+                <span>Inspiration</span>
+            </div>
+            <h2 class="text-base font-bold sm:text-lg md:text-xl"><a href="#_">Best Workstations of the Year</a></h2>
+            <p class="mt-2 text-sm text-gray-500">Check out these inspiring workstations to get ideas on how to level-up
+                your workstation.</p>
+        </div>
+    </div>
+</template>
